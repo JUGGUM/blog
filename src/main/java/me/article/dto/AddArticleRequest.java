8f39809279b82b5f1d3e5c3b5833271f0c3775d9
JUGGUM <gayoung801@naver.com>
2023-07-08 17:47:53 +0900

    Article Dto AddArticleRequest&ArticleResponse생성

---
+++ src/main/java/me/article/dto/AddArticleRequest.java
@@ -0,0 +1,21 @@
+package me.article.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.article.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+    private String title;
+    private String content;
+
+    // 빌더 패턴을 사용해 DTO를 엔티티로 만들어주는 메소드
+    public Article toEntity() {
+        return Article.builder()
+                .title(title)
+                .content(content).build();
+    }
+}
