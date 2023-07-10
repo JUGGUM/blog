65761bebf348c3aa8bec45718b61d16a9636dc05
JUGGUM <gayoung801@naver.com>
2023-07-10 16:48:07 +0900

    Blog Controller Service Repostory생성

---
+++ src/main/java/me/article/controller/BlogApiController.java
@@ -0,0 +1,63 @@
+package me.article.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.article.domain.Article;
+import me.article.dto.AddArticleRequest;
+import me.article.dto.ArticleResponse;
+import me.article.dto.UpdateArticleRequest;
+import me.article.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BlogApiController {
+    private final BlogService blogService;
+
+    //요청이 오면 응답용 객체인 ArticleResponse에 파싱해 body에 담아 클라이언트에게 전송
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        //스트림사용
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    //URL경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id){
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article saveArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(saveArticle);
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request) {
+        Article updatedArticle = blogService.update(id, request);
+        return ResponseEntity.ok()
+                .body(updatedArticle);
+    }
+
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+        return ResponseEntity.ok()
+                .build();
+    }
+}
---
+++ src/main/java/me/article/service/BlogService.java
@@ -0,0 +1,45 @@
+package me.article.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import me.article.domain.Article;
+import me.article.dto.AddArticleRequest;
+import me.article.dto.UpdateArticleRequest;
+import me.article.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+import java.beans.Transient;
+import java.util.List;
+
+@RequiredArgsConstructor //final 이 붙거나 @NotNull 이 붙은 필드의 생성자 추가
+@Service //빈으로 등록
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+        //AddArticleRequest 에 저장된 값들을 article 데이터베이스에 저
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not Found : " + id));
+    }
+
+    public void delete(long id){
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional // 트랜잭션 메서드 엔티티의 필드값이 바뀌면 중간에 에러가 발생해도 제대로 된 값 수정을 보장
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Not Fount : " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
