5908bee03ea7a9b97ddae10dec5e3dbd45f5d7b2
JUGGUM <gayoung801@naver.com>
2023-08-17 12:22:58 +0900

    Update : AddArticleRequest

--- src/main/java/me/article/dto/AddArticleRequest.java
+++ src/main/java/me/article/dto/AddArticleRequest.java
@@ -9,13 +9,16 @@ import me.article.domain.Article;
 @AllArgsConstructor
 @Getter
 public class AddArticleRequest {
-    private String title;
-    private String content;
 
-    // 빌더 패턴을 사용해 DTO를 엔티티로 만들어주는 메소드
-    public Article toEntity() {
-        return Article.builder()
-                .title(title)
-                .content(content).build();
-    }
+  private String title;
+  private String content;
+
+  // 빌더 패턴을 사용해 DTO를 엔티티로 만들어주는 메소드
+  public Article toEntity(String author) {
+    return Article.builder()
+        .title(title)
+        .content(content)
+        .author(author)
+        .build();
+  }
 }
