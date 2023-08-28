fc5852901588724038fd542fe89b570b9e3215cf
JUGGUM <gayoung801@naver.com>
2023-08-28 11:57:12 +0900

    Style : google java 스타일 지정

--- src/main/java/me/user/controller/UserViewController.java
+++ src/main/java/me/user/controller/UserViewController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserViewController {
     @GetMapping("/login")
-    public String login(){
-        return "login";
+    public String login() {
+        return "oauthLogin";
     }
+
     @GetMapping("/signup")
     public String signup() {
         return "signup";
