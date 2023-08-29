9b3539a64bcbfda4e94b4f757c05ef1eece8ab69
JUGGUM <gayoung801@naver.com>
2023-08-30 01:18:35 +0900

    Update : BlogService authorizeArticleAuthor추가
    - 게시글을 작성한 유저인지 확인합니다.

--- src/main/java/me/article/service/BlogService.java
+++ src/main/java/me/article/service/BlogService.java
@@ -42,4 +42,13 @@ public class BlogService {
 
         return article;
     }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().
+                getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
+    }
 }
