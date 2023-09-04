a07623e20410e7327f6052e779a9969623ec5724
JUGGUM <gayoung801@naver.com>
2023-09-04 11:40:13 +0900

    Style : BlogApiControllerTest

--- src/test/java/me/article/controller/BlogApiControllerTest.java
+++ src/test/java/me/article/controller/BlogApiControllerTest.java
@@ -39,7 +39,7 @@ class BlogApiControllerTest {
     protected MockMvc mockMvc;
 
     @Autowired
-    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
+    protected ObjectMapper objectMapper;  // 직렬화, 역직렬화를 위한 클래스
     @Autowired
     private WebApplicationContext context;
     @Autowired
