c95bd5a8a0e4281abd14e797cb760862bc85c785
JUGGUM <gayoung801@naver.com>
2023-09-14 16:46:40 +0900

    Update : UserService

--- src/main/java/me/user/service/UserService.java
+++ src/main/java/me/user/service/UserService.java
@@ -4,19 +4,40 @@ import lombok.RequiredArgsConstructor;
 import me.user.domain.User;
 import me.user.dto.AddUserRequest;
 import me.user.repository.UserRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Long save(AddUserRequest dto){
-        return userRepository.save(User.builder()
-                .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
-                .build()).getId();
-    }
+  private final UserRepository userRepository;
+//  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+//  public Long save(AddUserRequest dto) {
+//    return userRepository.save(User.builder()
+//        .email(dto.getEmail())
+//        .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+//        .build()).getId();
+//  }
+
+  public Long save(AddUserRequest dto) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    return userRepository.save(User.builder()
+        .email(dto.getEmail())
+        .password(encoder.encode(dto.getPassword()))
+        .build()).getId();
+  }
+
+  public User findByEmail(String email) {
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+  }
+
+
+  public User findById(Long userId) {
+    return userRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("Unexpected User"));
+  }
 }
