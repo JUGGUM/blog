5617d22f92e6af83204d3e5dddb0ff76e39dfee7
JUGGUM <gayoung801@naver.com>
2023-10-05 14:17:48 +0900

    Feat : FileController 파일업로드 구현

---
+++ src/main/java/me/utils/file/FileController.java
@@ -0,0 +1,28 @@
+package me.utils.file;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+@RestController
+@Slf4j
+public class FileController {
+
+    // 파일 업로드 구현
+    @PostMapping("/api/uploadFile")
+    public ResponseEntity<String> uploadFile(MultipartFile file) throws IllegalStateException, IOException {
+
+        if (!file.isEmpty()) {
+            log.debug("file org name = {}", file.getOriginalFilename());
+            log.debug("file content type = {}", file.getContentType());
+            file.transferTo(new File(file.getOriginalFilename()));
+        }
+
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+}
